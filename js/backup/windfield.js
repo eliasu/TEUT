2b615cff3c3a316f6ff41222c3696ddfd0258c73
Eliasu <elias@commandg.de>
2025-04-09 11:49:59 +0200

    Init Repo

---
+++ js/backup/windfield.js
@@ -0,0 +1,186 @@
+// Global settings (shared across all instances)
+const perlin_noise = noise;
+
+let detail = 10;
+let rotation_factor = 1;
+let noise_density = 0.001;
+let noise_multiplier = 0.005;
+
+let length_min = 0.5;
+let length_max = 0.8;
+let weight_min = 2;
+let weight_max = 10;
+
+let size = 35;
+
+let stroke_color = '#36ABDB';
+
+
+function getResponsiveSize(baseSize) {
+    const width = window.innerWidth;
+
+    if (width < 768) {
+        return 25;
+    } else if (width < 992) {
+        return 28;
+    } else {
+        return baseSize;
+    }
+}
+
+class Windfield {
+    constructor(container) {
+        this.container = container;
+        this.canvas = null;
+        this.lines = [];
+        this.frame_number = 1;
+        this.play = this.container.getAttribute("wf_play") === "true" ? true : false;
+        this.still = !this.play;
+        this.wf_size = getResponsiveSize(size);
+        this.stroke_color = this.container.getAttribute("wf_stroke_color") || stroke_color;
+
+        this.initP5();
+    }
+
+    // create a P5 canvas inside the container
+    initP5() {
+        new p5((p) => {
+            this.p = p;
+
+            p.setup = () => this.setup();
+            p.draw = () => this.draw();
+            p.windowResized = () => this.windowResized();
+        }, this.container);
+    }
+
+    setup() {
+        this.wf_size = getResponsiveSize(size);
+        this.container.style.background = "transparent";
+        this.detail = Math.round(this.container.clientWidth / this.wf_size);
+        this.canvas = this.p.createCanvas(this.container.clientWidth, this.container.clientHeight);
+        
+        if ( this.play ) {
+            this.setupObserver();
+        }
+
+        // if its still, only call draw once
+        if (this.still) {
+            this.draw();
+            this.p.noLoop();
+        }
+    } 
+
+    // stop the windfield, if its not in view, play it if in view
+    setupObserver() {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                this.play = entry.isIntersecting;
+                if (this.play) {
+                    this.p.loop();
+                } else {
+                    this.p.noLoop();
+                }
+            });
+        }, { threshold: 0.1 });
+
+        observer.observe(this.container);
+    }
+
+    windowResized() {
+        this.p.resizeCanvas(this.container.clientWidth, this.container.clientHeight);
+        this.wf_size = getResponsiveSize(size); // or re-read attribute if you use it
+        this.draw();
+    }
+
+    draw() {
+        this.p.clear();
+        this.p.background('rgba(100%, 100%, 100%, 0)');
+
+        if (this.play) {
+            this.frame_number++;
+        }
+
+        let new_width = this.p.width;
+        let new_height = this.p.height;
+        this.detail = Math.round(this.container.clientWidth / this.wf_size);
+        let columns = this.detail;
+        let rect_size = new_width / this.detail;
+        let rows = Math.floor(new_height / rect_size);
+        let offset_x = (this.p.width - rect_size * columns) / 2;
+        let offset_y = (this.p.height - rect_size * rows) / 2;
+
+        this.p.strokeCap(this.p.SQUARE);
+        this.p.noFill();
+
+        this.lines = [];
+
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < columns; c++) {
+                this.lines.push(new Line(this.p, rect_size * c + offset_x, rect_size * r + offset_y, rect_size, this.frame_number, this.stroke_color));
+            }
+        }
+
+        this.lines.forEach(l => l.draw());
+    }
+}
+
+class Line {
+    constructor(p, x, y, rect_size, frame_number, stroke_color) {
+        this.p = p;
+        this.x = x;
+        this.y = y;
+        this._x = this.x * noise_density;
+        this._y = this.y * noise_density;
+        this.rect_size = rect_size;
+        this.frame_number = frame_number; // Now it's passed into the class
+        this.stroke_color = stroke_color; // Stroke color is passed here
+    }
+
+    draw() {
+        const noiseValue = perlin_noise.simplex3(this._x, this._y, this.frame_number * noise_multiplier);
+        this.p.push();
+        this.p.translate(this.x + this.rect_size / 2, this.y + this.rect_size / 2);
+        this.p.rotate(this.p.map(noiseValue, -1, 1, 0, this.p.TWO_PI * rotation_factor));
+
+        const weight = this.p.map(noiseValue, -1, 1, weight_min, weight_max);
+        const len = this.p.map(noiseValue, -1, 1, length_min, length_max);
+
+        this.p.strokeWeight(weight);
+        this.p.stroke(this.stroke_color); // Use the passed stroke color
+        this.p.line(-this.rect_size * 0.5 * len, 0, this.rect_size * 0.5 * len, 0);
+
+        this.p.pop();
+    }
+}
+
+// Initialize all Windfield instances
+document.addEventListener('DOMContentLoaded', function () {
+    // Init the variables   
+    length_min = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--_windfield---wf_length_min').trim());
+    length_max = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--_windfield---wf_length_max').trim());
+    weight_min = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--_windfield---wf_weight_min').trim());
+    weight_max = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--_windfield---wf_weight_max').trim());
+    rotation_factor = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--_windfield---wf_rotation').trim());
+    
+    // Handle CSS variable updates globally
+    const root = document.documentElement;
+    const observer = new MutationObserver(() => {
+        length_min = parseFloat(getComputedStyle(root).getPropertyValue('--_windfield---wf_length_min').trim());
+        length_max = parseFloat(getComputedStyle(root).getPropertyValue('--_windfield---wf_length_max').trim());
+        weight_min = parseFloat(getComputedStyle(root).getPropertyValue('--_windfield---wf_weight_min').trim());
+        weight_max = parseFloat(getComputedStyle(root).getPropertyValue('--_windfield---wf_weight_max').trim());
+        rotation_factor = parseFloat(getComputedStyle(root).getPropertyValue('--_windfield---wf_rotation').trim());
+    });
+
+    observer.observe(root, { attributes: true, attributeFilter: ['style'] });
+    
+    // Initialize all Windfield instances
+    const wf_containers = document.querySelectorAll(".wf_container");
+    const windfields = [];
+
+    // Initialize all Windfield instances if there are multiple on the page
+    wf_containers.forEach(container => {
+        windfields.push(new Windfield(container));
+    });
+
+});
---
+++ js/landing.js
@@ -0,0 +1,108 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+   // Get the zoom levels from the map element's attributes (without 'data-' prefix)
+   const mapElement = document.querySelector("#map");
+   const initialZoom = parseInt(mapElement.getAttribute("initialzoom"), 10) || 6; // Default to 6 if not set
+   const styleUrl = mapElement.getAttribute("map_style_url");
+
+   const bounds = new mapboxgl.LngLatBounds();
+   const markers = new Map(); // Store markers for easy reference
+
+
+   // The value for 'accessToken' begins with 'pk...'
+   mapboxgl.accessToken = 'pk.eyJ1IjoiZWlsZWUiLCJhIjoiY203MXcwYzE3MDJzbjJrc2U2NXJvdnhpdSJ9.JD2SHZ83QNAMt_KZeO7ZXA';
+
+   const map = new mapboxgl.Map({
+      container: 'map',
+      style: styleUrl,
+      center: [-87.661557, 41.893748],
+      zoom: 10.7
+   });
+
+   map.addControl(new mapboxgl.FullscreenControl());
+
+   // ad markers and popups
+   document.querySelectorAll(".map_item").forEach(item => {
+      const lng = parseFloat(item.getAttribute("lng"));
+      const lat = parseFloat(item.getAttribute("lat"));
+      const classname = mapElement.getAttribute("classname") || 'test-class';
+      
+      const map_modal = item.querySelector(".map_modal");
+      const modalContent = map_modal?.innerHTML || "<p>No content available</p>";
+
+      if (!isNaN(lng) && !isNaN(lat)) {
+         // Create a popup and insert the copied content
+         const popup = new mapboxgl.Popup({ offset: 32, maxWidth: '18rem' })
+            .setHTML(modalContent) // Copy inner content into the popup
+            .addClassName(classname); // Dynamic class
+         
+         // create a HTML element for each feature
+         const el = document.createElement('div');
+         el.className = 'map_modal_marker';
+         
+         // Create a marker
+         const marker = new mapboxgl.Marker(el)
+            .setLngLat([lng, lat])
+            .setPopup(popup) // Attach the popup
+            .addTo(map);
+
+         // Store marker and popup for later reference
+         markers.set(item, { marker, popup });
+
+         // Move map to the marker when the popup opens
+         popup.on('open', () => {
+            map.flyTo({
+               center: [lng, lat],
+               padding: { bottom: 220 },
+               essential: true
+            });
+         });
+
+         bounds.extend([lng, lat]);
+      } else {
+         console.warn("Invalid coordinates for:", item);
+      }
+
+      // add popup interaction
+      item.addEventListener("click", () => {
+         const data = markers.get(item);
+         
+         if (data) {
+            const { marker, popup } = data;
+
+            // Fly to the clicked pin and wait for the flyTo to finish
+            map.flyTo({
+               center: marker.getLngLat(),
+               padding: { bottom: 220 },
+               essential: true
+            });
+            
+            // remove all already opened popups
+            const popups = document.getElementsByClassName("mapboxgl-popup");
+
+            if (popups.length) {
+               popups[0].remove();
+            }
+
+            // Wait for the 'moveend' event after flyTo animation is finished
+            map.once('moveend', () => {
+               // After the flyTo animation finishes, open the popup
+               popup.addTo(map);
+            });
+         }
+      });
+
+      // remove the item from DOM
+      map_modal.remove();
+
+   });
+
+   // Adjust map view to fit all markers initially and avoid fade effect
+   if (!bounds.isEmpty()) {
+      // Set the initial position and zoom without the fade
+      map.jumpTo({
+         center: bounds.getCenter(),
+         padding: { bottom: 220 },
+         zoom: initialZoom // Use the initial zoom level from the #map element
+      });
+   }
+});
